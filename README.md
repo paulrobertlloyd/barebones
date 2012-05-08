4d4806fe7d7f77958af93cb845f0b6f93ee9b291
Paul Robert Lloyd <github@paulrobertlloyd.com>
2012-05-08 02:10:48 +0100

    Initial commit of Barebones files

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-barebones
-=========
+# Barebones
 
-Intial directory setup, styleguide and pattern primer. Intended as a starting point for my own projects, available for others to clone and adapt for theirs.
+Initial directory setup, styleguide and pattern primer. Intended as a starting point for my own projects, available for others to clone and adapt for theirs.
+
+## Licence
+
+This work is available under a Attribution-NonCommercial-ShareAlike 2.0 UK: England & Wales Creative Commons licence:
+<http://creativecommons.org/licenses/by-nc-sa/2.0/uk>
+
+## Referenced Works
+
+* Styleguide based on the Mezzoblue Markup Guide <http://www.mezzoblue.com/downloads/markupguide> 
+  by Dave Shea (<http://mezzoblue.com>)  
+  Licence: <http://creativecommons.org/licenses/by-sa/2.0/ca>
+
+* Patterns inspired and adapted from those of Pears (<http://pea.rs>)  
+  by Dan Cederholm (<http://simplebits.org>)  
+  Licence: <http://creativecommons.org/licenses/by-nc-sa/3.0>
+  
+* Pattern Primer (<https://github.com/adactio/Pattern-Primer>)  
+  by Jeremy Keith (<http://adactio.com>)
---
+++ patterns.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html><html lang="en-gb">
+
+<head>
+    <title>Pattern Primer</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="/_css/reset.css" type="text/css"/>
+    <link rel="stylesheet" href="/_css/patterns.css" type="text/css"/>
+    <style>
+        .pattern {
+            margin:2em 0;
+            border-top:1px dotted #d0d0d0;
+            padding:1.9375em 0 2em;
+            }
+            .pattern:before, .pattern:after {
+                content:"";
+                display:table;
+            }
+            .pattern:after {
+                clear:both;
+            }
+        .pattern .display {
+            width:55%;
+            float:left;
+        }
+        .pattern .source {
+            width:35%;
+            float:right;
+        }
+        .pattern .source textarea {
+            font:0.75em/1.5 Menlo,Monaco,'Courier New',Courier,monospace; /* 16px/24px */
+            width:100%;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Pattern Primer</h1>
+    <p class="lede">A guide to all the common snippets of markup used throughout the site.</p>
+
+<?php
+    $files = array();
+    $patterns_dir = "_patterns";
+    $handle = opendir($patterns_dir);
+    while (false !== ($file = readdir($handle))):
+        if(stristr($file,'.html')):
+            $files[] = $file;
+        endif;
+    endwhile;
+    sort($files);
+    foreach ($files as $file):
+        echo '<div class="pattern">';
+        echo '<div class="display">';
+        include($patterns_dir.'/'.$file);
+        echo '</div>';
+        echo '<div class="source">';
+        echo '<textarea rows="10" cols="30">';
+        echo htmlspecialchars(file_get_contents($patterns_dir.'/'.$file));
+        echo '</textarea>';
+        echo '<p><a href="'.$patterns_dir.'/'.$file.'">'.$file.'</a></p>';
+        echo '</div>';
+        echo '</div>';
+    endforeach;
+?>
+    <footer role="contentinfo">
+        <nav role="navigation">
+            <ul>
+                <li><a href="index.php">Styleguide</a></li>
+                <li><a href="patterns.php">Pattern Primer</a></li>
+                <li><a href="readme.md">Read Me</a></li>
+            </ul>
+        </nav>
+        <p><small>Copyright &#169; 2012 Paul Robert Lloyd. This work is <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.0/uk/">Creative Commons licensed</a>.</small></p>
+    </footer><!--/.contentinfo-->
+</body>
+</html>
