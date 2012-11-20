9236f8209c0917f8ad808acd565edce991aad0a0
Paul Lloyd <github@paulrobertlloyd.com>
2012-11-20 21:39:55 +0000

    Add index page, linking to styleguide and pattern portfolio

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html><html lang="en-gb"<?php if(isset($_GET['debug'])) echo ' class="debug"';?>>
+
+<head>
+    <title>Style Guide</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="_css/reset.css" type="text/css"/>
+    <link rel="stylesheet" href="_css/patterns.css" type="text/css"/>
+</head>
+
+<body>
+    <h1>Barebones</h1>
+    <p>Initial directory setup, style guide and pattern primer. Intended as a starting point for my own projects, available for others to fork and adapt for theirs.</p>
+    <p>Lovingly crafted by <a href="http://paulrobertlloyd.com/">Paul Robert Lloyd</a>.</p>
+
+    <h2>Contents</h2>
+    <dl>
+        <dt><a href="_styleguide.php">Styleguide</a></dt>
+        <dd>A guide to the base markup styles used throughout the site.</dd>
+        <dt><a href="_styleguide.php">Pattern Library</a></dt>
+        <dd>A guide to all the common snippets of markup used throughout the site.</dd>
+    </dl>
+</body>
+</html>
